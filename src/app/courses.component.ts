9da76f287f7268260dca533e85693c20510f3cce
s2danii <s2d.shen@gmail.com>
2019-11-07 10:41:16 -0500

    authors exercise

---
+++ src/app/courses.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
+
+@Component({
+    selector: 'courses',
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `
+})
+
+export class CoursesComponent {
+    title = 'List of courses';
+    courses;
+
+    getTitle() {
+        return `Title: ${this.title}`;
+    }
+
+    constructor (service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+}
